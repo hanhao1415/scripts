e4066eb8b403b356b8f014b156c132d055814f1c
Tyler Blazey <tblaze11@gmail.com>
2011-06-28 15:32:00 -0500

    added three simple scripts for dti qc

---
+++ dti_motion_outliers.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+import csv,sys
+
+#import motion paramaters file and skip the header row
+motion_file = csv.reader(open(sys.argv[1],'r'),delimiter=' ',quotechar="'",quoting=csv.QUOTE_NONNUMERIC)
+motion_file.next()
+
+#put each column into a list
+frame,disx,disy,disz,rotx,roty,rotz = zip(*motion_file)
+
+#loop through every frame, checking for translation and rotation outliers
+dis_outliers = 0; rot_outliers = 0
+for x_dis,y_dis,z_dis,x_rot,y_rot,z_rot in zip(disx,disy,disz,rotz,roty,rotz):
+	if abs(x_dis)>=1 or abs(y_dis)>=1 or abs(z_dis)>=1:
+		dis_outliers += 1
+	if abs(x_rot)>=.5 or abs(y_rot)>=.5 or abs(z_rot)>=.5:
+		rot_outliers += 1
+
+print dis_outliers,rot_outliers
