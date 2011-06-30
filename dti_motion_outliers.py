a93f1186605865c8cee2a71635aad1b36177b0e7
Tyler Blazey <tblaze11@gmail.com>
2011-06-30 14:56:49 -0500

    Add fsl and qa to dti processing...

--- dti_motion_outliers.py
+++ dti_motion_outliers.py
@@ -7,13 +7,12 @@ motion_file.next()
 
 #put each column into a list
 frame,disx,disy,disz,rotx,roty,rotz = zip(*motion_file)
-
 #loop through every frame, checking for translation and rotation outliers
 dis_outliers = 0; rot_outliers = 0
 for x_dis,y_dis,z_dis,x_rot,y_rot,z_rot in zip(disx,disy,disz,rotz,roty,rotz):
-	if abs(x_dis)>=1 or abs(y_dis)>=1 or abs(z_dis)>=1:
+	if abs(float(x_dis))>=1 or abs(float(y_dis))>=1 or abs(float(z_dis))>=1:
 		dis_outliers += 1
-	if abs(x_rot)>=.5 or abs(y_rot)>=.5 or abs(z_rot)>=.5:
+	if abs(float(x_rot))>=.5 or abs(float(y_rot))>=.5 or abs(float(z_rot))>=.5:
 		rot_outliers += 1
 
 print dis_outliers,rot_outliers
