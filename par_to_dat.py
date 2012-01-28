4fbb4307aa678490b6c92330a3b72d9363c98df3
Tyler Blazey <tblaze11@gmail.com>
2012-01-27 23:53:30 -0600

    Getting really close

--- par_to_dat.py
+++ par_to_dat.py
@@ -16,7 +16,7 @@ arg_parse.add_argument('outroot',help='Root for outputed files',nargs=1)
 #Optional arguments
 arg_parse.add_argument('-mask',help='Masked used for calculating mean intensity.',nargs=1,default=[0])
 arg_parse.add_argument('-rel',help='Output tracjectory relative to run mean. Same as mat2dat -R option',action='store_const',default=[0],const=[1])
-arg_parse.add_argument('-dis',help='Save differentiated trajectory. Same as mat2dat -D option.',action='store_const',default=[0],const=[1])
+arg_parse.add_argument('-diff',help='Save differentiated trajectory. Same as mat2dat -D option.',action='store_const',default=[0],const=[1])
 arg_parse.add_argument('-skip',help='Number of frames to skip. Default is 3. Same as mat2dat -n option.',type=int,default=[3],nargs=1)
 arg_parse.add_argument('-rad',help='Radius used to calculated total rms. Default is 50.',type=float,default=[50.0],nargs=1)
 arg_parse.add_argument('-ref',help='Reference frame for scale calculation. Default is 1.',type=int,default=[1],nargs=1)
@@ -30,18 +30,20 @@ def param_sum(filename,params):
 	mean = np.ma.mean(params,axis=0)
 	stdev = np.ma.std(params,axis=0,ddof=1)
 	#Get rotational and translational rms
-	dis_rms = np.ma.sqrt(np.ma.sum(np.ma.power(stdev[1:4],2)))
+	trans_sum = np.ma.sum(np.ma.power(stdev[1:4],2))
+	trans_rms = np.ma.sqrt(trans_sum)
 	rot_rms = np.ma.sqrt(np.ma.sum(np.ma.power(stdev[4:7],2)))
 	#Get total rms
-	total_rms = np.ma.sqrt(np.ma.power(rot_rms*args.rad[0],2)+np.ma.power(dis_rms,2)) #This is wrong currently.
+	rot_rad_sum = np.sum(np.ma.power(np.ma.std(params[:,4:7]*(np.arctan(1)/45),axis=0,ddof=1),2))
+	total_rms = np.ma.sqrt(trans_sum+(np.ma.power(args.rad[0],2)*rot_rad_sum))
 	
 	#Write out results 
 	file.write('#Counting %i out of %i frames\n'%(params.shape[0]-args.skip[0],params.shape[0]))
-	file.write('#Mean: % .5f % .5f % .5f % .5f % .5f % .5f % .5f\n'%(mean[1],mean[2],mean[3],mean[4],mean[5],mean[6],mean[7]))
-	file.write('#Stdv: % .5f % .5f % .5f % .5f % .5f % .5f % .5f\n'%(stdev[1],stdev[2],stdev[3],stdev[4],stdev[5],stdev[6],stdev[7]))
-	file.write('#Dis RMS: % .5f\n'%(dis_rms))
-	file.write('#Rot RMS: % .5f\n'%(rot_rms))
-	file.write('#Total RMS at radius %.3f: %.5f\n'%(args.rad[0],total_rms))
+	file.write('#mean  % .5f % .5f % .5f % .5f % .5f % .5f % .5f\n'%(mean[1],mean[2],mean[3],mean[4],mean[5],mean[6],mean[7]))
+	file.write('#s.d.  % .5f % .5f % .5f % .5f % .5f % .5f % .5f\n'%(stdev[1],stdev[2],stdev[3],stdev[4],stdev[5],stdev[6],stdev[7]))
+	file.write('#rms translation (mm) % .5f\n'%(trans_rms))
+	file.write('#rms rotation (deg) % .5f\n'%(rot_rms))
+	file.write('#total rms movement at radius=%.3fmm %.5f\n'%(args.rad[0],total_rms))
 	return
 
 #Function to output graphs
@@ -62,14 +64,14 @@ def param_graph(type,params):
 
 	#Translation plot
 	fig2 = plt.figure(2)
-	dis = fig2.add_subplot(1,1,1)
-	dis.plot(params[:,3:6])
-	dis.set_xlim([0,dims[0]])
-	dis.set_xlabel('Frame',fontsize=14)
-	dis.set_ylabel('Rotations (degrees)',fontsize=14)
-	dis.set_title(type + ': MCFLIRT Estimated Rotations',fontsize=14)
-	pos = dis.get_position(); dis.set_position([pos.x0, pos.y0, pos.width * 0.95, pos.height])
-	dis.legend(('X', 'Y', 'Z'),'upper right', shadow=True,title='Axis',bbox_to_anchor=(1.17,1))
+	trans = fig2.add_subplot(1,1,1)
+	trans.plot(params[:,3:6])
+	trans.set_xlim([0,dims[0]])
+	trans.set_xlabel('Frame',fontsize=14)
+	trans.set_ylabel('Rotations (degrees)',fontsize=14)
+	trans.set_title(type + ': MCFLIRT Estimated Rotations',fontsize=14)
+	pos = trans.get_position(); trans.set_position([pos.x0, pos.y0, pos.width * 0.95, pos.height])
+	trans.legend(('X', 'Y', 'Z'),'upper right', shadow=True,title='Axis',bbox_to_anchor=(1.17,1))
 	fig2.savefig(args.outroot[0] + '_' + type + '_rot.png')
 	fig2.clf()
 
@@ -97,38 +99,52 @@ except (IOError):
 	print 'Error: Cannot load motion parameters at %s'%(args.par[0])
 	sys.exit()
 
-#Setup masking
+#Setup image masking
 if args.mask[0] == 0:
-	#Get rid of zeros
-else
-	#Load in mask
+	image_mask_data = aligned_data <= 0
+else:
+	try:
+		input_mask = nib.load(args.mask[0])
+	except:
+		print 'Error: Cannot load mask at %s'(args.mask[0])
+		sys.exit()
+	input_mask_data = ( np.float64(input_mask.get_data()) - 1 ) * -1 #have to invert, as numpy masks out 1s and includes 0s
+	image_mask_data = np.empty_like(aligned_data)
+	image_mask_data = np.repeat(np.expand_dims(input_mask_data,axis=3),aligned_data.shape[3],axis=3)
 
 #Check inputs
 if args.skip[0] >= aligned_data.shape[3]:
 	print 'Error: Skip number cannot exceed or equal frame count.'
 	sys.exit()
-if raw.get_shape() != aligned.get_shape(): #add mask here
+if raw.get_shape() != aligned.get_shape():
 	print 'Error: Mismatch in image dimensions. Check data.'
 	sys.exit()
 if raw.get_shape()[3] != dims[0]:
-	print 'Error: Motion parameters and image dimensions due not match'
+	print 'Error: Motion parameters and image dimensions do not match'
 	sys.exit()
 if motion_params.shape[1] != 6:
 	print 'Error: Motion parameters do not have the expected six columns'
 	sys.exit()
-if args.ref[0] >= args.skip[0]:
+if args.ref[0] <= args.skip[0]:
 	print 'Error: Reference frame is within skip.'
 	sys.exit()
 
+#Mask images
+raw_masked_data = np.ma.array(raw_data,mask=image_mask_data)
+aligned_masked_data = np.ma.array(aligned_data,mask=image_mask_data)
+
 #Calculate intensity scaling
-ref_avg = np.mean(raw_data[:,:,:,args.ref[0]-1])
-scale = np.divide(np.apply_over_axes(np.average,aligned_data,[0,1,2]),ref_avg).flatten().reshape((dims[0],1))
+ref_avg = np.ma.mean(raw_masked_data[:,:,:,args.ref[0]-1])
+frame_avgs = np.empty((dims[0],1))
+for frame in range(aligned_data.shape[3]): #Loop here until apply_over_axes properly handles masks
+	frame_avgs[frame,:] = np.ma.mean(aligned_masked_data[:,:,:,frame])
+scale = np.divide(ref_avg,frame_avgs)
 
 #Get frame numbers
 frames = np.arange(1,dims[0]+1).reshape((dims[0],1))
 
 #Setup dat: add in frame count, convert radians to degrees and flip rotations and translations.
-dat = np.concatenate((frames,motion_params[:,3:6],motion_params[:,0:3]*57.2957795,scale),axis=1)
+dat = np.concatenate((frames,motion_params[:,3:6],motion_params[:,0:3]*(45/np.arctan(1)),scale),axis=1)
 
 #Generate masks, then apply to dat
 mask = np.concatenate((np.greater_equal(np.arange(0,args.skip[0]),0),np.less(np.arange(args.skip[0],dat.shape[0]),0))).reshape(dat.shape[0],1)
@@ -138,13 +154,13 @@ dat_masked = np.ma.array(dat,mask=params_mask)
 #Mask input images
 #Write out a .dat type file
 file = open(args.outroot[0] + '.dat','w')
-file.write('%s  %s\n'%('#',' '.join(sys.argv)))
+file.write('#%s\n'%(' '.join(sys.argv)))
 file.write('%-6s %-8s %7s %8s %8s %8s %8s %8s'%('#frame',' dX(mm)','dY(mm)','dZ(mm)','X(deg)','Y(deg)','Z(deg)','Scale\n'))
 np.savetxt(file,dat_masked,fmt=['%6i','% .5f','% .5f','% .5f','% .5f','% .5f','% .5f','% .5f'])
 param_sum(file,dat_masked); file.close()
 if args.plot[0] == 1: param_graph('dat',dat[:,1:7])
 
-if args.dis[0] == 1:
+if args.diff[0] == 1:
 	#Calculate framewise differences. Add back in first frame. Multiple scale by 100.
 	diff = np.diff(dat[:,1:8],axis=0); diff = np.insert(diff,0,0.0,axis=0); diff[:,6] = diff[:,6] * 100.0
 	
@@ -152,23 +168,23 @@ if args.dis[0] == 1:
 	ddat = np.concatenate((frames,diff),axis=1)
 	ddat_masked = np.ma.array(ddat,mask=params_mask)
 	file = open(args.outroot[0] + '.ddat','w'); 
-	file.write('%s %s\n'%('#',' '.join(sys.argv)))
-	file.write('%6s %-9s %7s %8s %9s %8s %8s %9s'%('#Frame',' ddX(mm)','ddY(mm)','ddZ(mm)','dX(deg)','dY(deg)','dZ(deg)',' 100*Scale\n'))
+	file.write('#%s\n'%(' '.join(sys.argv)))
+	file.write('%6s %-9s %7s %8s %8s %8s %8s %9s'%('#Frame',' ddX(mm)','ddY(mm)','ddZ(mm)','dX(deg)','dY(deg)','dZ(deg)',' 100*Scale\n'))
 	np.savetxt(file,ddat,fmt=['%6i','% .5f','% .5f','% .5f','% .5f','% .5f','% .5f','% .5f'])
 	param_sum(file,ddat_masked); file.close()
 	if args.plot[0] == 1: param_graph('ddat',ddat[:,1:7])
 
 if args.rel[0] == 1:
 	#Subtract mean motion paramaters
-	param_mean = np.mean(dat_masked[:,1:7],axis=0); param_rel = dat[:,1:7] - param_mean
-	scale_mean = np.ma.mean(np.ma.array(np.apply_over_axes(np.mean,raw_data,[0,1,2]),mask=mask))
-	scale_rel = np.divide(np.apply_over_axes(np.average,aligned_data,[0,1,2]),scale_mean).flatten().reshape((dims[0],1))
+	param_mean = np.ma.mean(dat_masked[:,1:7],axis=0); param_rel = dat[:,1:7] - param_mean
+	scale_mean = np.ma.mean(dat_masked[:,7])
+	scale_rel = np.divide(dat[:,7],scale_mean).reshape(param_rel.shape[0],1)
 	
 	#Writeout .rdat file
 	rdat = np.concatenate((frames,param_rel,scale_rel),axis=1)
 	rdat_masked = np.ma.array(rdat,mask=params_mask)
 	file = open(args.outroot[0] + '.rdat','w');
-	file.write('%s  %s\n'%('#',' '.join(sys.argv)))
+	file.write('#%s\n'%(' '.join(sys.argv)))
 	file.write('%-6s %-8s %7s %8s %8s %8s %8s %8s'%('#frame',' dX(mm)','dY(mm)','dZ(mm)','X(deg)','Y(deg)','Z(deg)','Scale\n'))
 	np.savetxt(file,rdat,fmt=['%6i','% .5f','% .5f','% .5f','% .5f','% .5f','% .5f','% .5f'])
 	param_sum(file,rdat_masked); file.close()
